189a1cf5e692ad5cd29d5d7649e0d603035716fb
Banana <banana.computermonkey@gmail.com>
2020-07-05 22:13:52 -0700

    Working password generator. needs finishing touches

---
+++ Develop/script.js
@@ -0,0 +1,59 @@
+
+var generateBtn = document.querySelector("#generate");
+
+
+function writePassword() {
+  var charValue = prompt("How many characters?");
+  //Verification to see if user has input a number between 8-128//
+  if(charValue<8 || charValue>128) {
+    return writePassword();
+  }
+  // Questions to request specifications from user//
+  var number = confirm('numbers?');
+  var specChar = confirm('Special Characters');
+  var lowerCase = confirm('lower case letters');
+  var upperCase = confirm('upper case letters');
+
+  //Arrays of character sets
+  var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  
+  var specCharArray =  ['!', '"','#', '$', '%', '&', '(',')','*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+  
+  var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+  var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  
+  //Empty array to push into//
+  var collectionArray=[];
+  
+  //Empty array to put randomized data from collectionArray//
+  var passwordArray=[]
+  
+  //Conditionals that recognize user input//
+  if (number){
+    collectionArray.push(...numberArray);
+   
+  }
+  if (specChar){
+    collectionArray.push(...specCharArray);
+   
+  }
+  if (lowerCase){
+    collectionArray.push(...lowerCaseArray);
+   
+  }
+  if (upperCase){
+    collectionArray.push(...upperCaseArray);
+    }
+// Loop to randomize the parts of the collectionArray to a length chosen by the user//
+  for (var i = 0; i < charValue; i++) {
+  passwordArray.push(collectionArray[Math.floor(Math.random()*collectionArray.length)]);
+  console.log(passwordArray);
+  }
+  //Attach array to HTML element #password and return value of passwordArray with no ',' or spaces//
+  const passwordBox= document.getElementById('password');
+  passwordBox.value=passwordArray.join(' ').replace(/ /g, "");
+}
+
+//Event listener, on click of button user signals to call function writePassword//
+generateBtn.addEventListener("click", writePassword);
